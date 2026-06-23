e56e1999c6cd0e9bc7fabfaff12da2a038da7f35
Lucas Picoli <LucasPicoli@users.noreply.github.com>
2026-06-23 10:16:09 -0700

    match: +1 function (func_00321268)

--- src/cod/000000.c
+++ src/cod/000000.c
@@ -3168,3 +3168,4 @@ INCLUDE_ASM("nonmatching", SearchData);
 
 
 
+
---
+++ src/cod/00321268.c
@@ -0,0 +1,7 @@
+/* sn-2.95.3-136 matched TU. */
+
+__attribute__((section(".text.func_00321268")))
+void *func_00321268(void)
+{
+  return *(void **)func_00320360();
+}
